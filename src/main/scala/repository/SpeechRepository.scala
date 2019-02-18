7e0019a0e5c12f982cfeda1f1c358acb905ae0dc
glueware <joerg.woelfel@glueware.com>
2019-02-18 10:50:49 +0100

    improved SpeechRepository

--- src/main/scala/repository/SpeechRepository.scala
+++ src/main/scala/repository/SpeechRepository.scala
@@ -16,36 +16,41 @@ trait Source[F[_]] {
 
   type G[_]
 
-  def list: G[Speech]
+  def findAll: G[Speech]
 
   def score(query: Query): F[ScoringMap]
 
-  protected def fold(scoringMap: ScoringMap, speech: Speech): ScoringMap = ???
+  protected def op(scoringMap: ScoringMap, speech: Speech): ScoringMap = ???
 }
 
 object ExecutionSpeechRepository extends SpeechRepositoryAlg[IO] {
-  class _Source() extends Source[IO] {
+
+  class _Source(urL: URL) extends Source[IO] {
     type G[A] = Stream[IO, A]
 
-    def list: G[Speech] = ???
+    def findAll: G[Speech] = ???
 
-    def score(query: Query): IO[ScoringMap] = ???
+    def score(query: Query): IO[ScoringMap] =
+      findAll.compile.fold(ScoringMap.empty(query))(op)
   }
 
   def apply(urL: URL): Source[IO] = ???
 }
 
 object TestSpeechRepository extends SpeechRepositoryAlg[Id] {
+
   class _Source(testList: List[Speech]) extends Source[Id] {
     type G[A] = List[A]
 
-    def list: G[Speech] = testList
+    def findAll: G[Speech] = testList
 
     def score(query: Query): Id[ScoringMap] =
-      testList.foldLeft(ScoringMap.empty(query))(fold)
+      testList.foldLeft(ScoringMap.empty(query))(op)
   }
 
   def apply(urL: URL): Source[Id] = {
+    val someTestData: List[Speech] = ???
     new _Source(List())
   }
+
 }
